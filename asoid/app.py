e73d62e0cc64ec02e8d78e6b139ad6e7d2ce002b
tillmannj <jens.tillmann@dzne.de>
2025-02-19 07:53:05 +0100

    hotfixed breaking changes from newer versions of streamlit

--- asoid/app.py
+++ asoid/app.py
@@ -173,7 +173,6 @@ def main():
                 </style>
                 """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-    st.set_option('deprecation.showPyplotGlobalUse', False)
     st.markdown(
         """
         <style>
